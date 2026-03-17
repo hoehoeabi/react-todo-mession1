519a27ca9d5fa393799f64e4ee62b1950c845df6
hoehoeabi <0203jyh00@naver.com>
2026-03-17 22:15:06 +0900

    chore: 리액트 초기 환경 세팅 완료
    처음엔 순수 JS로만 만드는 줄 알았음

--- src/App.js
+++
@@ -1,8 +0,0 @@
-// App 컴포넌트를 가져옵니다. (바닐라 JS는 확장자 .js 필수!)
-import App from "./App.js";
-
-// 1. html에서 id가 root인 태그를 낚아챕니다.
-const $root = document.getElementById("root");
-
-// 2. App을 생성하면서 root 태그를 던져줍니다.
-new App({ $target: $root });
